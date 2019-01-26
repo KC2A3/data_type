e7a0b1cfc57db7c82050ccc7005e413d7e49cc9e
KC2A3 <947496351@qq.com>
2019-01-27 03:44:36 +0800

    Define default values

---
+++ src/dataType.java
@@ -0,0 +1,10 @@
+ public class dataType {
+        public static byte defaultByte;
+        public static short defaultShort;
+        public static int defaultInt;
+        public static long defaultLong;
+        public static float defaultFloat;
+        public static double defaultDouble;
+        public static boolean defaultBoolean;
+        public static char defaultChar;
+}
