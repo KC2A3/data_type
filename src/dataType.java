7d3d2e9db809114ffc2897df742ebe4e8023b98a
KC2A3 <947496351@qq.com>
2019-01-27 03:46:57 +0800

    Print minimum, maximum, and default values

--- src/dataType.java
+++ src/dataType.java
@@ -7,4 +7,14 @@
         public static double defaultDouble;
         public static boolean defaultBoolean;
         public static char defaultChar;
+        public static void main(String[] args){
+                 System.out.println("byte类型最小值是"+Byte.MIN_VALUE+"最大值是"+Byte.MAX_VALUE+"默认值是"+defaultByte);
+                 System.out.println("short类型最小值是"+Short.MIN_VALUE+"最大值是"+Short.MAX_VALUE+"默认值是"+defaultShort);
+                 System.out.println("int类型最小值是"+Integer.MIN_VALUE+"最大值是"+Integer.MAX_VALUE+"默认值是"+defaultInt);
+                 System.out.println("long类型最小值是"+Long.MIN_VALUE+"最大值是"+Long.MIN_VALUE+"默认值是"+defaultLong);
+                 System.out.println("float类型最小值是"+Float.MIN_VALUE+"最大值是"+Float.MAX_VALUE+"默认值是"+defaultFloat);
+                 System.out.println("double类型最小值是"+Double.MIN_VALUE+"最大值是"+Double.MAX_VALUE+"默认值是"+defaultDouble);
+                 System.out.println("boolean类型最小值是"+Boolean.FALSE+"最大值是"+Boolean.TRUE+"默认值是"+defaultBoolean);
+                 System.out.println("char类型最小值是"+Character.MIN_VALUE+"最大值是"+Character.MAX_VALUE+"默认值是"+defaultChar);
+         }
 }
